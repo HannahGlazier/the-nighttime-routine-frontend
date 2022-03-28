c995bc17af8836c4e06617228e4cf88a4cf1e5c9
Hannah Glazier <hrayglazier@aol.com>
2022-03-28 17:08:31 -0600

    initial component setup

--- nighttime-routine-frontend/src/App.js
+++ nighttime-routine-frontend/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
+import { Route, Switch } from "react-router-dom";
+import Header from './components/Header'
+import NavBar from './components/NavBar'
+import ProductList from './components/ProductList'
+import Wishlist from './components/Wishlist'
+import AddProduct from './components/AddProduct'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        <NavBar />
+        <Switch>
+          <Route exact path='/products' >
+            <ProductList />
+          </Route>
+          <Route exact path='/wishlist' >
+            <Wishlist />
+          </Route>
+          <Route exact path='/addProduct' >
+            <AddProduct />
+          </Route>
+        </Switch>
     </div>
   );
 }
---
+++ nighttime-routine-frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>The Nighttime Routine</div>
+  )
+}
+
+export default Header
